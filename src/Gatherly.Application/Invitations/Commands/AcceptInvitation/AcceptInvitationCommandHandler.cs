fe3421f4dd884447c3c94519cbe11244891a28f9
Michael Maurice <M.Maurice@klickit.co>
2024-12-21 23:13:08 +0200

    Aggregate Root

--- src/Gatherly.Application/Invitations/Commands/AcceptInvitation/AcceptInvitationCommandHandler.cs
+++ src/Gatherly.Application/Invitations/Commands/AcceptInvitation/AcceptInvitationCommandHandler.cs
@@ -32,23 +32,23 @@ internal sealed class AcceptInvitationCommandHandler : IRequestHandler<AcceptInv
 
     public async Task<Unit> Handle(AcceptInvitationCommand request, CancellationToken cancellationToken)
     {
-        var invitation = await _invitationRepository
-            .GetByIdAsync(request.InvitationId, cancellationToken);
+       
+        var gathering = await _gatheringRepository
+            .GetByIdWithCreatorAsync(request.GatheringId, cancellationToken);
+
+        if ( gathering is null)
+        {
+            return Unit.Value;
+        }
+        var invitation = gathering.Invitations.FirstOrDefault(i=>i.Id==request.InvitationId);
 
         if (invitation is null || invitation.Status != InvitationStatus.Pending)
         {
             return Unit.Value;
         }
 
-        var member = await _memberRepository.GetByIdAsync(invitation.MemberId, cancellationToken);
+       // var member = await _memberRepository.GetByIdAsync(invitation.MemberId, cancellationToken);
 
-        var gathering = await _gatheringRepository
-            .GetByIdWithCreatorAsync(invitation.GatheringId, cancellationToken);
-
-        if (member is null || gathering is null)
-        {
-            return Unit.Value;
-        }
 
         var attendee = gathering.AcceptInvitation(invitation);
 
---
+++ src/Gatherly.Domain/Errors/AggregateRoot.cs
@@ -0,0 +1,12 @@
+﻿
+
+using Gatherly.Domain.Primitives;
+
+namespace Gatherly.Domain.Errors;
+
+public abstract class AggregateRoot : Entity
+{
+    protected AggregateRoot(Guid id) : base(id)
+    {
+    }
+}
